eb8a135461336b2f27cb4e0be4c6579e14e93c39
mcclayac <anthony.mcclay@me.com>
2018-11-18 16:52:17 -0600

    Added Jinga templates and home.html

--- module1/heaadlines.py
+++ module1/heaadlines.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 import feedparser
 
 app = Flask(__name__)
@@ -8,24 +9,29 @@ BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 @app.route("/")
 def get_news():
     feed = feedparser.parse(BBC_FEED)
+    first_article = feed['entries'][0]
+    return render_template("home.html",
+                            title=first_article.get("title"),
+                            published = first_article.get("published"),
+                            summary = first_article.get("summary"))
 
-    html = []
-    html.append("""<html><body>""")
-    entries = feed['entries']
-    for e in entries:
-        article="""
-            <h1> BBC Headlines </h1>
-            <b>{0}</b> <br/>
-            <i>{1}</i> <br/>
-            <p>{2}</p> <br/>""".format(e.get("title"),
-                                       e.get("published"),
-                                       e.get("summary"))
-
-        html.append(article)
-    html.append("""</body>
-         </html>""")
-    result = ''.join(html)
-    return result
+    # html = []
+    # html.append("""<html><body>""")
+    # entries = feed['entries']
+    # for e in entries:
+    #     article="""
+    #         <h1> BBC Headlines </h1>
+    #         <b>{0}</b> <br/>
+    #         <i>{1}</i> <br/>
+    #         <p>{2}</p> <br/>""".format(e.get("title"),
+    #                                    e.get("published"),
+    #                                    e.get("summary"))
+    #
+    #     html.append(article)
+    # html.append("""</body>
+    #      </html>""")
+    # result = ''.join(html)
+    # return result
 
 
 if __name__ == '__main__':
